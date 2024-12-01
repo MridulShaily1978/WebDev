ad9bf592010dfd023ffe62f842b63577ae26fe59
MridulShaily1978 <co23346@ccet.ac.in>
2024-12-01 12:58:21 +0530

    Add files via upload

---
+++ EventManagementSystems/utils/footer.php
@@ -0,0 +1,38 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>C</strong><span class = "small footerSubtext">handigarh</span>
+                        <strong>C</strong><span class = "small footerSubtext">ollege of</span>
+                        <strong>E</strong><span class = "small footerSubtext">ngineering and</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        sector 26, chandigarh India <br>
+                        &copy; Urban Events Venue & Catering, 2024.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    DL824 Multimedia Design/Web Engineering<br>
+                    Creative Computing 2023-2027.<br>
+                    Manojji
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4 "><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/instagram.png" width = 80>
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
